6cadc7a4147e88db808b75a24bdf33aae5dce738
rpeloso21 <rpeloso21@gmail.com>
2024-11-30 07:08:50 -0500

    final commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,7 @@ class TestAPI(unittest.TestCase):
         payload = {'num1':5, 'num2':-7}
         response = self.app.post('/sum', json=payload)
         data = response.get_json()
-        self.assertEqual(data['result'], -1)
+        self.assertEqual(data['result'], -2)
 
 if __name__ == "__main__":
     unittest.main()
